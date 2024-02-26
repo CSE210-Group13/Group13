3be96e5c306d8dbd5b5ccd0978dcb6b088541c42
Han Chen <chenhan@uw.edu>
2024-02-26 12:20:06 -0800

    sample home page

---
+++ Webpages/javascript/home.js
@@ -0,0 +1,9 @@
+function markComplete() {
+    // Implement the completion logic
+    alert('Challenge completed!');
+  }
+  
+  function refreshChallenge() {
+    // Implement the refresh logic
+    alert('Challenge refreshed!');
+  }
